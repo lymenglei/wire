9899e36adc5c4c52afdc24ac218834a20e7b1028
lymenglei <ly-menglei@163.com>
2018-04-02 17:36:03 +0800

    add run-*.sh

---
+++ ml/export/run-client.sh
@@ -0,0 +1,1 @@
+java -classpath server-client.jar:../lib/wire-compiler-2.3.0-RC1-jar-with-dependencies.jar network.Client -structName=ABC -structCount=1 -host=localhost -port=55533
---
+++ ml/export/run-server.sh
@@ -0,0 +1,1 @@
+java -classpath server-client.jar:../lib/wire-compiler-2.3.0-RC1-jar-with-dependencies.jar network.Server
